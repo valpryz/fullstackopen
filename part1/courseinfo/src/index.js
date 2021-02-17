0f317e5716c1fc4303cba2ad92610cf79cd8fbad
valpryz <valpryz@gmail.com>
2021-02-17 14:02:24 +0100

    exercise 1.5 completed

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -20,8 +20,9 @@ const Total = (props) => <p>Number of exercises {props.parts[0].exercises + prop
 
 const App = () => {
   
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name : 'Fundamentals of React',
       exercises: 10
@@ -35,12 +36,13 @@ const App = () => {
       exercises: 14
     }
   ]
+  }
   
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }

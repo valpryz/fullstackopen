24be7c78df594767fe6fd5f6f5d365a7e1a91f18
valpryz <valpryz@gmail.com>
2021-03-03 18:20:35 +0100

    exercise 2.6 step1 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const person = {
+      name: newName,
+      id: newName,
+    };
+    if (newName) {
+      setPersons(persons.concat(person));
+      setNewName("");
+    }
+  };
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((p) => (
+          <p key={p.id}>{p.name}</p>
+        ))}
+      </div>
     </div>
   );
 };

fa854a260f0a455099a096da0d811304ef11630c
valpryz <valpryz@gmail.com>
2021-03-16 11:18:13 +0100

    exercise 2.12 step1 completed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const findCountries = countries.filter((country) => {
+    return country.name.toLowerCase().includes(search);
+  });
+
+  const OneCountry = ({ country }) => {
+    return (
+      <div>
+        <h1>{country.name}</h1>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {country.languages.map((language) => (
+            <li key={language.nativeName}>{language.name}</li>
+          ))}
+        </ul>
+        <img src={country.flag} alt={`${country.name} flag`} width="150" />
+      </div>
+    );
+  };
+
+  const ShownCountries = ({ countries }) => {
+    if (!search) {
+      return null;
+    }
+
+    if (countries.length > 10) {
+      return <p>Too many matches, specify another filter</p>;
+    }
+
+    return (
+      <div>
+        {countries.map((country) => (
+          <p key={country.alpha3Code}>{country.name}</p>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>
+        find countries
+        <input value={search} onChange={handleSearch} />
+      </label>
+      {findCountries.length === 1 ? (
+        <OneCountry country={findCountries[0]} />
+      ) : (
+        <ShownCountries countries={findCountries} />
+      )}
     </div>
   );
-}
+};
 
 export default App;

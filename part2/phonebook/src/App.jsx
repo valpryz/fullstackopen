622d1b278c45f07abfdeffab4c1c6feea5169034
valpryz <valpryz@gmail.com>
2025-01-02 11:27:02 +0100

    exercise 2.11 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 const Filter = (props) => {
   const {search, onSearch} = props
@@ -37,17 +38,19 @@ const Persons = (props) => {
   </div>
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState("")
   const [search, setSearch] = useState("")
   const addName = (e) => setNewName(e.target.value)
   const addPhone = (e) => setNewPhone(e.target.value)
+
+  const fetchPersons = () => {
+    axios.get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data))
+  }
+
+  useEffect(fetchPersons, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()

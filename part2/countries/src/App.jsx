2e930aaff28b4dc170392e72e8c8eb556e2ed46c
valpryz <valpryz@gmail.com>
2025-01-22 14:14:25 +0100

    exercise 2.19 refactored & completed

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,55 +1,69 @@
-import {useState, useEffect} from 'react'
-import axios from "axios"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Country = (props) => {
-  const {country} = props
-  return (
-    <div>
-      <h2>{country.name.common}</h2>
-      <p>capital {country.capital[0]}</p>
-      <p>area {country.area}</p>
-      <h4>languages :</h4>
-      <ul>
-        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-      </ul>
-      <img src={country.flags.png} alt={country.flags.alt} />
-    </div>
-  )
+const CountryLine = (props) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const {country} = props
+    return (
+        <div>
+            <div>{country.name.common} <button onClick={()=> setIsOpen(!isOpen)}>{isOpen ? "hide" : "show"}</button></div>
+            {isOpen && <CountryView country={country}/>}
+        </div>
+    )
+}
+
+const CountryView = (props) => {
+    const {country} = props
+
+    return (
+        <div>
+            <h2>{country.name.common}</h2>
+            <p>capital {country.capital}</p>
+            <p>area {country.area}</p>
+            <h3>languages:</h3>
+            <ul>
+                {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+            </ul>
+            <img src={country.flags.png} alt={country.flags.alt}/>
+        </div>
+    )
+}
+
+const Countries = (props) => {
+
+    const {countries} = props
+
+    if(countries.length > 10) {
+        return <div>Too many matches, specify another filter</div>
+    }
+
+    if(countries.length > 1 && countries.length <= 10) {
+        const countriesListing = countries.map(country =>  <CountryLine key={country.area} country={country} />)
+        return countriesListing
+    }
+
+    if(countries.length === 1) return <CountryView country={countries[0]}/>
 }
 
 const App = () => {
-  const [countries, setCountries] = useState([])
-  const [search, setSearch] = useState('')
-  const [id, setId] = useState("")
+    const [searchCountry, setSearchCountry] = useState('')
+    const [countries, setCountries] = useState([])
 
-  const fetchCountries = () => {
-    axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then(response => setCountries(response.data))
-  }
+    const getCountries = () => {
+        axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+            .then(response => setCountries(response.data))
+    }
 
-  const countryFilter = () => {
-    const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
+    useEffect(getCountries, [])
 
-    if(filteredCountries.length > 10) return "Too many matches, specify another filter"
+    const onSearch = (e) => setSearchCountry(e.target.value)
 
-    if(filteredCountries.length <= 10 && filteredCountries.length > 1) {
-      return  filteredCountries.map(country => <div key={country.name.common}>{country.name.common} {id !== country.ccn3 && <button onClick={() => setId(country.ccn3)}>{id === country.ccn3 ? "hide": "show"}</button>}
-        {id === country.ccn3 ? <Country country={country} key={country.name.common} /> : null}
-      </div>)     
-  }
+    const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(searchCountry.toLowerCase()))
 
-    if(filteredCountries.length === 1) return <Country country={filteredCountries[0]} key={filteredCountries.ccn3}/>
-    
-  }
-
-  useEffect(fetchCountries, [])
-
-  return (
-    <>
-     <div>find countries <input value={search} onChange={(e) => setSearch(e.target.value)} /></div> 
-     <div>{countryFilter()}</div>
+    return <>
+        <div>find countries <input type="text" value={searchCountry} onChange={onSearch}/></div>
+        <Countries countries={filteredCountries}/>
     </>
-  )
 }
 
 export default App
